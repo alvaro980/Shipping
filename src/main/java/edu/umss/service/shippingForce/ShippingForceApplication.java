250278fd58221346cebe5452843bfe8f10aed3c4
Alvaro Pereira <alvaro.pereira@trueforce.com>
2019-06-12 10:33:23 -0400

    project inicialized

---
+++ src/main/java/edu/umss/service/shippingForce/ShippingForceApplication.java
@@ -0,0 +1,13 @@
+package edu.umss.service.shippingForce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingForceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/umss/service/shippingForce/ShippingForceApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.umss.service.shippingForce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShippingForceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
